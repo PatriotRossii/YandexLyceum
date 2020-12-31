0b6ee0421867613366b830e12713ae81849d8e03
PatriotRossii <patriotrossii2019@mail.ru>
2020-12-31 13:23:14 +0500

    Implement update film dialogue functionality by extending AddFilmDialogue

--- QT 8. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 2/Filmotheque1.0/main.py
+++ QT 8. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 2/Filmotheque1.0/main.py
@@ -11,40 +11,82 @@ class NumItem(QTableWidgetItem):
         return int(self.text()) < int(other.text())
 
 
-class AddFilmDialogue(QDialog):
-    def __init__(self):
+class AddUpdateFilmDialogue(QDialog):
+    def __init__(self, update=False, film_id=None):
         super().__init__()
+
+        self.film_id = film_id
+        self.update = update
 
         self.con = sqlite3.connect("films_db.sqlite")
         self.cur = self.con.cursor()
 
         uic.loadUi("add_dialogue.ui", self)
+
         self.genre_edit.addItems(
             [e[0] for e in self.cur.execute("SELECT title FROM genres").fetchall()]
         )
+        if update:
+            self.submit_btn.setText("Обновить")
+            self.init_fields(self.get_data())
 
         self.init_signals()
 
+    def init_fields(self, data):
+        self.title_edit.setText(data["title"])
+        self.year_edit.setText(str(data["year"]))
+        self.genre_edit.setCurrentText(data["genre"])
+        self.duration_edit.setText(str(data["duration"]))
+
+    def get_data(self):
+        data = self.cur.execute("SELECT films.title, year, genres.title, duration FROM films INNER JOIN genres ON "
+                                "films.genre = genres.id WHERE films.id = ?", [self.film_id]).fetchone()
+        return {"title": data[0], "year": data[1], "genre": data[2], "duration": data[3]}
+
     def get_fields(self):
-        return {"title": self.title_edit.text(), "year": self.year_edit.text(), "genre": self.genre_edit.currentText(),
+        row_id = self.cur.execute("select max(id) from films").fetchone()[0] + 1 if not self.update else self.film_id
+        genre_id = self.cur.execute("SELECT id FROM genres WHERE title = ?", [self.genre_edit.currentText()])\
+            .fetchone()[0]
+
+        return {"id": row_id, "title": self.title_edit.text(), "year": self.year_edit.text(), "genre": genre_id,
                 "duration": self.duration_edit.text()}
 
-    def insert_data(self, data):
+    def catch_errors(self, fn, data):
         if not all(data.values()):
             self.indicator_lbl.setText("Неверно заполнена форма")
         try:
-            row_id = self.cur.execute("select max(id) from films").fetchone()[0] + 1
-            genre_id = self.cur.execute("SELECT id FROM genres WHERE title = ?", [data["genre"]]).fetchone()[0]
-            self.cur.execute("INSERT INTO films(id, title, year, genre, duration) VALUES(?, ?, ?, ?, ?)", [
-                row_id, data["title"], data["year"], genre_id, data["duration"]
-            ])
-            self.con.commit()
-            self.accept()
+            fn(self, data)
         except sqlite3.Error:
             self.indicator_lbl.setText("Неверные данные")
 
+    def insert_data(self, data):
+        def insert_fn(self, data):
+            self.cur.execute("INSERT INTO films(id, title, year, genre, duration) VALUES(?, ?, ?, ?, ?)", [
+                data["id"], data["title"], data["year"], data["genre"], data["duration"]
+            ])
+            self.con.commit()
+            self.accept()
+
+        self.catch_errors(
+            insert_fn, data
+        )
+
+    def update_data(self, data):
+        def update_fn(self, data):
+            self.cur.execute("UPDATE films SET title = ?, year = ?, genre = ?, duration = ? WHERE id = ?", [
+                data["title"], data["year"], data["genre"], data["duration"], data["id"]
+            ])
+            self.con.commit()
+            self.accept()
+        self.catch_errors(
+            update_fn, data
+        )
+
     def init_signals(self):
-        self.submit_btn.clicked.connect(lambda: self.insert_data(self.get_fields()))
+        if self.update:
+            self.submit_btn.clicked.connect(lambda: self.update_data(self.get_fields()))
+        else:
+            self.submit_btn.clicked.connect(lambda: self.insert_data(self.get_fields()))
 
 
 class Example(QMainWindow):
@@ -69,7 +111,7 @@ class Example(QMainWindow):
 
     def init_signals(self):
         self.add_film_btn.clicked.connect(self.open_add_film_dialogue)
-        #self.edit_film_btn.clicked.connect(self.open_edit_film_dialogue)
+        self.edit_film_btn.clicked.connect(self.open_edit_film_dialogue)
         self.remove_film_btn.clicked.connect(self.remove_film)
 
         #self.add_genre_btn.clicked.connect(self.open_add_genre_dialogue)
@@ -104,7 +146,18 @@ class Example(QMainWindow):
         self.con.close()
 
     def open_add_film_dialogue(self):
-        dialogue = AddFilmDialogue()
+        dialogue = AddUpdateFilmDialogue()
+        dialogue.exec_() and self.refresh_films_table()
+
+    def open_edit_film_dialogue(self):
+        current_row = self.films_table_widget.currentRow()
+
+        if current_row == -1:
+            return
+
+        film_id = self.films_table_widget.item(current_row, 0).text()
+
+        dialogue = AddUpdateFilmDialogue(update=True, film_id=film_id)
         dialogue.exec_() and self.refresh_films_table()
 
     def refresh_films_table(self):
